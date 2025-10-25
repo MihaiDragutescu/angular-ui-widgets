79e00f66764e872e37b22faee73d55603e4ad1e2
Mihai Dragutescu <mihaidragutescu36@gmail.com>
2025-10-25 19:06:10 +0300

    feature(progress-indicator): implement t-progress component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TProgress } from './components/t-progress/t-progress';
+
+const PROGRESS_STEP = 10;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [TProgress],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('angular-ui-widgets');
+  progress = signal(0);
+
+  increaseProgress() {
+    const updatedProgress = this.progress() >= 100 ? 0 : this.progress() + PROGRESS_STEP;
+    this.progress.set(updatedProgress);
+  }
+
+  onComplete() {
+    console.log('Progress complete!');
+  }
 }
---
+++ src/app/components/t-progress/t-progress.consts.ts
@@ -0,0 +1,2 @@
+export const MIN_RADIUS = 50;
+export const STROKE_WIDTH = 10;
