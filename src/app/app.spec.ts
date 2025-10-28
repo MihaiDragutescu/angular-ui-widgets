15b50cb9e242c5102228e79516361b9d88da9af8
Mihai Dragutescu <mihaidragutescu36@gmail.com>
2025-10-28 19:30:17 +0200

    test(app): fix sorting test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { Direction } from './components/t-grid/t-grid.consts';
+import { MOCK_DATA } from './app.consts';
 
 describe('App', () => {
   beforeEach(async () => {
@@ -19,6 +20,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
 
+    const originalData = [...MOCK_DATA];
+
     app.onSortChange({ columnName: 'firstName', direction: Direction.Asc });
     expect(app.myData[0].firstName <= app.myData[1].firstName).toBeTrue();
 
@@ -26,6 +29,6 @@ describe('App', () => {
     expect(app.myData[0].firstName >= app.myData[1].firstName).toBeTrue();
 
     app.onSortChange({ columnName: 'firstName', direction: Direction.None });
-    expect(app.myData).toEqual(app.myData);
+    expect(app.myData).toEqual(originalData);
   });
 });
